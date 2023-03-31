ecc04b0f833a5a9f73d7529d08f2127a74ace72e
Muhammad Abdullah <mabdullah15@hotmail.com>
2023-04-01 04:22:44 +0500

    Added sidebar animations

--- public/js/script.js
+++ public/js/script.js
@@ -6,23 +6,40 @@ const sidebarTitle = document.querySelector(".sidebar--menu--text");
 let sidebarState = "expanded";
 
 // updateSidebar();
-extendButton.classList.add("sidebarButtonsCollapsed");
+// extendButton.classList.add("sidebarButtonsCollapsed");
+extendButton.style.display = "none";
 
 function updateSidebar() {
-  if (sidebarState == "expanded") {
+  if (sidebarState == "collapsed") {
     console.log("Collapse");
-    sidebarState = "collapsed";
+
     sidebarButtonTexts.forEach((element) => {
       element.classList.add("sidebarButtonsCollapsed");
     });
     sidebarTitle.classList.add("sidebarButtonsCollapsed");
+    sidebarTitle.style.display = "none";
+
     sidebar.classList.remove("sidebarExpanded");
     sidebar.classList.add("sidebarCollapsed");
-    collapseButton.classList.add("sidebarButtonsCollapsed");
-    extendButton.classList.remove("sidebarButtonsCollapsed");
-  } else if (sidebarState == "collapsed") {
+    // collapseButton.classList.add("sidebarButtonsCollapsed");
+    collapseButton.style.display = "none";
+
+    // extendButton.classList.remove("sidebarButtonsCollapsed");
+    extendButton.style.display = "initial";
+  } else if (sidebarState == "expanded") {
     console.log("Expand");
-    sidebarState = "expanded";
+    sidebar.classList.remove("sidebarCollapsed");
+    sidebar.classList.add("sidebarExpanded");
+
+    //   collapseButton.classList.remove("sidebarButtonsCollapsed");
+    collapseButton.style.display = "initial";
+
+    //   extendButton.classList.add("sidebarButtonsCollapsed");
+    extendButton.style.display = "none";
+
+    sidebarTitle.classList.remove("sidebarButtonsCollapsed");
+    sidebarTitle.style.display = "initial";
+
     sidebarButtonTexts.forEach((element) => {
       element.classList.remove("sidebarButtonsCollapsed");
     });
@@ -30,5 +47,11 @@ function updateSidebar() {
 }
 
 collapseButton.onclick = function () {
+  sidebarState = "collapsed";
+  updateSidebar();
+};
+
+extendButton.onclick = function () {
+  sidebarState = "expanded";
   updateSidebar();
 };
