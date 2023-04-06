66661d5f87df98f677a141854d8d7b518fb66c9c
Muhammad Abdullah <mabdullah15@hotmail.com>
2023-04-07 03:51:15 +0500

    Started SIgn Up Page

--- public/js/signIn.js
+++ public/js/signIn.js
@@ -1,27 +1,47 @@
 const signInButtonSelector = document.getElementById("signInButtonSelector");
 const signUpButtonSelector = document.getElementById("signUpButtonSelector");
-const passwordReveal = document.getElementById("passwordReveal");
-const passwordInput = document.getElementById("passwordInput");
-const emailInput = document.getElementById("emailInput");
-const passwordErrorPrompt = document.getElementById("passwordErrorPrompt");
-// const signInButton = document.getElementById("signInButton");
-const form = document.getElementById("form");
+const signInPasswordReveal = document.getElementById("signInPasswordReveal");
+const signInPasswordInput = document.getElementById("signInPasswordInput");
+const signInEmailInput = document.getElementById("signInEmailInput");
+const signInPasswordErrorPrompt = document.getElementById(
+  "signInPasswordErrorPrompt"
+);
+
+const signUpPasswordReveal = document.getElementById("signUpPasswordReveal");
+const signUpConfirmPasswordReveal = document.getElementById(
+  "signUpConfirmPasswordReveal"
+);
+const signUpPasswordInput = document.getElementById("signUpPasswordInput");
+const signUpEmailInput = document.getElementById("signUpEmailInput");
+
+const signInForm = document.getElementById("signInForm");
+const signUpForm = document.getElementById("signUpForm");
 
 let URL = window.location.href.split("/", 3).join("/");
 
-passwordErrorPrompt.style.display = "none";
+signInPasswordErrorPrompt.style.display = "none";
+
 let selector = "signIn";
-let passwordRevealed = "hidden";
+let signInPasswordRevealed = "hidden";
+let signUpPasswordRevealed = "hidden";
+let signUpConfirmPasswordRevealed = "hidden";
+
 signUpButtonSelector.classList.add("hover-animation");
 updateSelector();
 
 function updateSelector() {
+  signInForm.reset();
+  signUpForm.reset();
   if (selector == "signIn") {
     signUpButtonSelector.classList.remove("selectorButtonSelected");
     signInButtonSelector.classList.add("selectorButtonSelected");
+    signUpForm.classList.remove("selected-form");
+    signInForm.classList.add("selected-form");
   } else if (selector == "signUp") {
     signInButtonSelector.classList.remove("selectorButtonSelected");
     signUpButtonSelector.classList.add("selectorButtonSelected");
+    signInForm.classList.remove("selected-form");
+    signUpForm.classList.add("selected-form");
   }
 }
 
@@ -41,16 +61,40 @@ signUpButtonSelector.onclick = function () {
   }
 };
 
-passwordReveal.onclick = function () {
-  console.log("toggle");
-  if (passwordRevealed == "revealed") {
-    passwordRevealed = "hidden";
-    passwordReveal.src = URL + "/eye-outline.svg";
-    passwordInput.type = "password";
+signInPasswordReveal.onclick = function () {
+  if (signInPasswordRevealed == "revealed") {
+    signInPasswordRevealed = "hidden";
+    signInPasswordReveal.src = URL + "/eye-outline.svg";
+    signInPasswordInput.type = "password";
   } else {
-    passwordRevealed = "revealed";
-    passwordReveal.src = URL + "/eye-off-outline.svg";
-    passwordInput.type = "text";
+    signInPasswordRevealed = "revealed";
+    signInPasswordReveal.src = URL + "/eye-off-outline.svg";
+    signInPasswordInput.type = "text";
+  }
+};
+
+signUpPasswordReveal.onclick = function () {
+  if (signUpPasswordRevealed == "revealed") {
+    signUpPasswordRevealed = "hidden";
+    signUpPasswordReveal.src = URL + "/eye-outline.svg";
+    signUpPasswordInput.type = "password";
+  } else {
+    signUpPasswordRevealed = "revealed";
+    signUpPasswordReveal.src = URL + "/eye-off-outline.svg";
+    signUpPasswordInput.type = "text";
+  }
+};
+
+signUpConfirmPasswordReveal.onclick = function () {
+  console.log("toggle");
+  if (signUpConfirmPasswordRevealed == "revealed") {
+    signUpConfirmPasswordRevealed = "hidden";
+    signUpConfirmPasswordReveal.src = URL + "/eye-outline.svg";
+    signUpConfirmPasswordInput.type = "password";
+  } else {
+    signUpConfirmPasswordRevealed = "revealed";
+    signUpConfirmPasswordReveal.src = URL + "/eye-off-outline.svg";
+    signUpConfirmPasswordInput.type = "text";
   }
 };
 
@@ -66,10 +110,10 @@ signUpButtonSelector.onmouseover = function () {
   }
 };
 
-form.addEventListener("submit", async (event) => {
+signInForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  email = emailInput.value;
-  password = passwordInput.value;
+  email = signInEmailInput.value;
+  password = signInPasswordInput.value;
 
   const result = await fetch("/login", {
     method: "POST",
@@ -84,36 +128,9 @@ form.addEventListener("submit", async (event) => {
     .then((res) => res.json())
     .then((res) => {
       if (res.error) {
-        passwordErrorPrompt.style.display = "initial";
+        signInPasswordErrorPrompt.style.display = "initial";
         return;
       }
       window.location.href = res.path;
     });
 });
-//   const response = await fetch("/checkPassword", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({
-//       password: passwordInput.value,
-//       email: emailInput.value,
-//     }),
-//   });
-//   const result = await response.text();
-//   console.log(result);
-//   if (result == "false") {
-//     passwordErrorPrompt.style.display = "initial";
-//   } else {
-//     location.href = URL + "/";
-//   }
-
-// signInButton.onclick = async function () {
-//   //   location.href = URL + "/";
-//   const plaintextPassword = document.getElementById("passwordInput").value;
-//   try {
-//     const hashedPassword = await bcrypt.hash(plaintextPassword, 10);
-//     console.log(hashedPassword);
-//     // Send the hashed password to the server using AJAX or form submission
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
